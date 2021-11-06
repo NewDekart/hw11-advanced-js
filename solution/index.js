085b2bec87345dd251d5d68a4b3cfa5d880fa998
Amal Dusalin <adusalin@fil-it.ru>
2021-11-06 21:29:38 +0300

    Прогнал prettier

--- solution/index.js
+++ solution/index.js
@@ -1,79 +1,78 @@
 module.exports = class {
+  constructor(initValue) {
+    this.order = [];
 
-    constructor(initValue) {
-        this.order = []
+    for (const val of initValue) {
+      this.add(val);
+    }
+  }
 
-        for (const val of initValue) {
-            this.add(val)
-        }
+  add(value) {
+    if (this.order.indexOf(value) === -1) {
+      this.order.push(value);
     }
 
-    add(value) {
-        if (this.order.indexOf(value) === -1) {
-            this.order.push(value)
-        }
+    return this;
+  }
 
-        return this
+  has(value) {
+    return this.order.indexOf(value) !== -1;
+  }
+
+  get size() {
+    return this.order.length;
+  }
+
+  [Symbol.iterator]() {
+    let index = 0;
+
+    return {
+      next: () => {
+        const result = {
+          value: this.order[index],
+          done: index >= this.size,
+        };
+
+        index += 1;
+
+        return result;
+      },
+    };
+  }
+
+  keys() {
+    return this.order.values();
+  }
+
+  values() {
+    return this.order.values();
+  }
+
+  *entries() {
+    const order = this.order;
+
+    for (const value of order) {
+      yield [value, value];
     }
+  }
 
-    has(value) {
-        return this.order.indexOf(value) !== -1
+  clear() {
+    this.order = [];
+  }
+
+  delete(value) {
+    const index = this.order.indexOf(value);
+
+    this.order = this.order.slice(0, index).concat(this.order.slice(index + 1));
+  }
+
+  get [Symbol.toStringTag]() {
+    return "^_^";
+  }
+
+  forEach(func, context = this) {
+    for (const value of this) {
+      func.bind(context)(value);
     }
-
-    get size() {
-        return this.order.length
-    }
-
-    [Symbol.iterator]() {
-        let index = 0
-
-        return {
-            next: () => {
-                const result = {
-                    value: this.order[index],
-                    done: index >= this.size
-                }
-
-                index += 1;
-
-                return result
-            }
-        }
-    }
-
-    keys() {
-        return this.order.values()
-    }
-
-    values() {
-        return this.order.values()
-    }
-
-    *entries() {
-        const order = this.order
-
-        for (const value of order) {
-            yield [value, value]
-        }
-    }
-
-    clear() {
-        this.order = []
-    }
-
-    delete(value) {
-        const index = this.order.indexOf(value)
-
-        this.order = this.order.slice(0, index).concat(this.order.slice(index + 1))
-    }
-
-    get [Symbol.toStringTag]() {
-        return '^_^';
-    }
-
-    forEach(func, context = this) {
-        for (const value of this) {
-            func.bind(context)(value)
-        }
-    }
-}
+  }
+};
--- tests/mySet.test.js
+++ tests/mySet.test.js
@@ -1,185 +1,193 @@
-const MySet = require('../solution')
+const MySet = require("../solution");
 
-describe('Проверка собственного множества', () => {
-    it('Хранит только уникальные значения', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+describe("Проверка собственного множества", () => {
+  it("Хранит только уникальные значения", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        expect([...mySet]).toEqual([...set])
-    })
+    expect([...mySet]).toEqual([...set]);
+  });
 
-    it('Свойство size присутствует и работает корректно', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Свойство size присутствует и работает корректно", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        expect(mySet.size).toBe(set.size)
-    })
+    expect(mySet.size).toBe(set.size);
+  });
 
-    it('Работает в цикле for-of', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Работает в цикле for-of", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const arrayFromMySet = []
-        const arrayFromNativeSet = []
+    const arrayFromMySet = [];
+    const arrayFromNativeSet = [];
 
-        for (const item of mySet) {
-            arrayFromMySet.push(item)
-        }
+    for (const item of mySet) {
+      arrayFromMySet.push(item);
+    }
 
-        for (const item of set) {
-            arrayFromNativeSet.push(item)
-        }
+    for (const item of set) {
+      arrayFromNativeSet.push(item);
+    }
 
-        expect(arrayFromMySet).toEqual(arrayFromNativeSet)
-    })
+    expect(arrayFromMySet).toEqual(arrayFromNativeSet);
+  });
 
-    it('Реализован и корректно работает метод keys', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод keys", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const arrayFromMySet = []
-        const arrayFromNativeSet = []
+    const arrayFromMySet = [];
+    const arrayFromNativeSet = [];
 
-        for (const item of mySet.keys()) {
-            arrayFromMySet.push(item)
-        }
+    for (const item of mySet.keys()) {
+      arrayFromMySet.push(item);
+    }
 
-        for (const item of set.keys()) {
-            arrayFromNativeSet.push(item)
-        }
+    for (const item of set.keys()) {
+      arrayFromNativeSet.push(item);
+    }
 
-        expect(arrayFromMySet).toEqual(arrayFromNativeSet)
-    })
+    expect(arrayFromMySet).toEqual(arrayFromNativeSet);
+  });
 
-    it('Реализован и корректно работает метод values', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод values", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const arrayFromMySet = []
-        const arrayFromNativeSet = []
+    const arrayFromMySet = [];
+    const arrayFromNativeSet = [];
 
-        for (const item of mySet.values()) {
-            arrayFromMySet.push(item)
-        }
+    for (const item of mySet.values()) {
+      arrayFromMySet.push(item);
+    }
 
-        for (const item of set.values()) {
-            arrayFromNativeSet.push(item)
-        }
+    for (const item of set.values()) {
+      arrayFromNativeSet.push(item);
+    }
 
-        expect(arrayFromMySet).toEqual(arrayFromNativeSet)
-    })
+    expect(arrayFromMySet).toEqual(arrayFromNativeSet);
+  });
 
-    it('Реализован и корректно работает метод entries', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод entries", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const arrayFromMySet = []
-        const arrayFromNativeSet = []
+    const arrayFromMySet = [];
+    const arrayFromNativeSet = [];
 
-        for (const item of mySet.entries()) {
-            arrayFromMySet.push(item)
-        }
+    for (const item of mySet.entries()) {
+      arrayFromMySet.push(item);
+    }
 
-        for (const item of set.entries()) {
-            arrayFromNativeSet.push(item)
-        }
+    for (const item of set.entries()) {
+      arrayFromNativeSet.push(item);
+    }
 
-        expect(arrayFromMySet).toEqual(arrayFromNativeSet)
-    })
+    expect(arrayFromMySet).toEqual(arrayFromNativeSet);
+  });
 
-    it('Реализован и корректно работает метод clear', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод clear", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        mySet.clear()
-        set.clear()
+    mySet.clear();
+    set.clear();
 
-        expect(mySet.size).toBe(set.size)
-    })
+    expect(mySet.size).toBe(set.size);
+  });
 
-    it('Реализован и корректно работает метод add', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод add", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const object = {
-            getValue () { return this.value }
-        }
+    const object = {
+      getValue() {
+        return this.value;
+      },
+    };
 
-        const data = {
-            value: 42
-        }
+    const data = {
+      value: 42,
+    };
 
-        set.add(object);
-        set.add(data);
+    set.add(object);
+    set.add(data);
 
-        mySet.add(object);
-        mySet.add(data)
+    mySet.add(object);
+    mySet.add(data);
 
-        expect([...mySet]).toEqual([...set])
-    })
+    expect([...mySet]).toEqual([...set]);
+  });
 
-    it('Метод add корректо работает в цепочке вызовов', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Метод add корректо работает в цепочке вызовов", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        const object = {
-            getValue () { return this.value }
-        }
+    const object = {
+      getValue() {
+        return this.value;
+      },
+    };
 
-        mySet.add(object).add(object).add(object).add('тест');
-        set.add(object).add(object).add(object).add('тест');
+    mySet.add(object).add(object).add(object).add("тест");
+    set.add(object).add(object).add(object).add("тест");
 
-        expect([...mySet]).toEqual([...set])
-    })
+    expect([...mySet]).toEqual([...set]);
+  });
 
-    it('Реализован и корректно работает метод delete', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод delete", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        mySet.delete(8)
-        set.delete(8)
+    mySet.delete(8);
+    set.delete(8);
 
-        expect([...mySet]).toEqual([...set])
-    })
+    expect([...mySet]).toEqual([...set]);
+  });
 
-    it('Реализован и корректно работает метод has', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
-        const set = new Set([4, 8, 15, 15, 16, 23, 42]);
+  it("Реализован и корректно работает метод has", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+    const set = new Set([4, 8, 15, 15, 16, 23, 42]);
 
-        expect(mySet.has({})).toBe(set.has({}))
-        expect(mySet.has(15)).toBe(set.has(15))
-        expect(mySet.has(84)).toBe(set.has(84))
-    })
+    expect(mySet.has({})).toBe(set.has({}));
+    expect(mySet.has(15)).toBe(set.has(15));
+    expect(mySet.has(84)).toBe(set.has(84));
+  });
 
-    it('Корректно работает valueOf', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+  it("Корректно работает valueOf", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
 
-        expect(mySet === mySet.valueOf()).toBeTruthy()
-    })
+    expect(mySet === mySet.valueOf()).toBeTruthy();
+  });
 
-    it('Корректно работает преобразование к строке', () => {
-        const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
+  it("Корректно работает преобразование к строке", () => {
+    const mySet = new MySet([4, 8, 15, 15, 16, 23, 42]);
 
-        expect(String(mySet)).toBe('[object ^_^]')
-        expect(Object.prototype.toString.call(mySet)).toBe('[object ^_^]')
-    })
+    expect(String(mySet)).toBe("[object ^_^]");
+    expect(Object.prototype.toString.call(mySet)).toBe("[object ^_^]");
+  });
 
-    it('Реализована и корректно работает функция forEach', () => {
-        const mySet = new MySet([4, 8]);
+  it("Реализована и корректно работает функция forEach", () => {
+    const mySet = new MySet([4, 8]);
 
-        const data = {
-            value: 42
-        }
+    const data = {
+      value: 42,
+    };
 
-        const mySetForEachResult = []
-        const dataForEachResult = []
+    const mySetForEachResult = [];
+    const dataForEachResult = [];
 
-        const mySetFunc = (item) => {mySetForEachResult.push(item)}
-        const dataFunc = function (item) {dataForEachResult.push(this.value)}
-        mySet.forEach(mySetFunc)
-        mySet.forEach(dataFunc, data)
+    const mySetFunc = (item) => {
+      mySetForEachResult.push(item);
+    };
+    const dataFunc = function (item) {
+      dataForEachResult.push(this.value);
+    };
+    mySet.forEach(mySetFunc);
+    mySet.forEach(dataFunc, data);
 
-        expect(mySetForEachResult).toEqual([4, 8])
-        expect(dataForEachResult).toEqual([42, 42])
-    })
-})
+    expect(mySetForEachResult).toEqual([4, 8]);
+    expect(dataForEachResult).toEqual([42, 42]);
+  });
+});
